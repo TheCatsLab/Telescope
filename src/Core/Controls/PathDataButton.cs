cf4a8408678930090b43a018c1c6b91226dce595
Dmitry Shevchuk <dshauchuk@gmail.com>
2022-10-11 18:46:38 +0300

    Implement PathDataButton

---
+++ src/Core/Controls/PathDataButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Cats.Telescope.VsExtension.Core.Controls;
+
+internal class PathDataButton : Button
+{
+    static PathDataButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(PathDataButton), new FrameworkPropertyMetadata(typeof(PathDataButton)));
+    }
+
+    #region Path Data
+
+    internal static readonly DependencyProperty PathDataProperty = DependencyProperty.Register(
+        "PathData", typeof(string), typeof(PathDataButton), new PropertyMetadata(null, OnPathDataChanged));
+
+    private static void OnPathDataChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+    }
+
+    public string PathData
+    {
+        get => (string)GetValue(PathDataProperty);
+        set => SetValue(PathDataProperty, value);
+    }
+
+    #endregion
+}
---
+++ src/Core/Converters/PathDataToGeometryConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Cats.Telescope.VsExtension.Core.Converters;
+internal class PathDataToGeometryConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string pathData || pathData is null)
+            return null;
+
+        return Geometry.Parse(pathData);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
